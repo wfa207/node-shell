7461469e65a4acf41321cf598c57675b0b9fbc41
Wes <wfa207@gmail.com>
2016-04-25 16:07:43 -0400

    added bash.js file

---
+++ bash.js
@@ -0,0 +1,6 @@
+mkdir node-shell # create a new directory named `node-shell`
+cd node-shell # switch into this directory
+touch bash.js # create a new file
+git add bash.js
+git commit -m "added bash.js file"
+git log # shows the commit you just made
