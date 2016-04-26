1dafd0ab92351ec5e58bde41b373b6b1c630f1e8
Wes <wfa207@gmail.com>
2016-04-26 10:49:48 -0400

    set up commands.js

--- bash.js
+++ bash.js
@@ -1,6 +1,18 @@
-mkdir node-shell # create a new directory named `node-shell`
-cd node-shell # switch into this directory
-touch bash.js # create a new file
-git add bash.js
-git commit -m "added bash.js file"
-git log # shows the commit you just made
+var commands = require('./commands');
+var fs = require('fs');
+var request = require('request');
+
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var input= data.toString().trim().split(" ");
+  var cmd = input[0]; // remove the newline
+
+  if (commands[cmd]) {
+  	commands[cmd](input);
+  } else {
+  	process.stdout.write('Invalid command!');
+  }
+});
